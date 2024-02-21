f93a0915f4a6cf0e08586fbb5286d18fd5bfd1dd
Salvador-a <salvador.sar614@outlook.com>
2024-02-20 18:37:02 -0600

    Se creo footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -10,30 +10,30 @@
             </p>
         </div>
         <nav class="menu-redes"> 
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100080663501910">
                 <span class="menu-redes__ocultar">Facebook</span>
             </a> 
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/SalvadorAc46573">
                 <span class="menu-redes__ocultar">Twitter</span>
             </a> 
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/channel/UCqkJQdyA2rJptnzfjSbk54g">
                 <span class="menu-redes__ocultar">YouTube</span>
             </a> 
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/salvadorasrqq/">
                 <span class="menu-redes__ocultar">Instagram</span>
             </a> 
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.tiktok.com/@salvador_acostaro">
                 <span class="menu-redes__ocultar">Tiktok</span>
             </a> 
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="h">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/Salvador-a">
                 <span class="menu-redes__ocultar">GitHub</span>
             </a>
         </nav>
     </div>
 
-    <p class="footer__copyright">
+    <p class="footer__copyrigth">
         DevWebCamp
-        <span class="footer__copyriht--regular">
+        <span class="footer__copyrigth--regular">
             - Todos los derechos reservados <?php echo date('Y'); ?>
         </span>
 

d7d75237f1aeaed61e9873c331e51dbf28ad1cf9
Salvador-a <salvador.sar614@outlook.com>
2024-02-24 15:22:53 -0600

    Salvador el campo de Area de Experencia

--- src/js/tags.js
+++ src/js/tags.js
@@ -9,6 +9,13 @@
 
         let tags = [];
 
+        // Recuperar del input oculto
+        if(tagsInputHidden.value !== '') {
+            tags = tagsInputHidden.value.split(',');
+            mostrarTags();
+        }
+
+
         // Escuchar los cambios en el input
         tagsInput.addEventListener('keypress', guardarTag);
 

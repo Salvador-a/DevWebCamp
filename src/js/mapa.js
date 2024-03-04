f6e741bed78c4b90ceb8b1ac47de57dbb005298c
Salvador-a <salvador.sar614@outlook.com>
2024-03-04 13:26:56 -0600

    Se creo la perte de pontes y la secion de mapa

---
+++ src/js/mapa.js
@@ -0,0 +1,23 @@
+if (document.querySelector("#mapa")) {
+
+  const lat =  19.432264
+  const lng = -98.983563
+  const zoom = 20
+  
+    
+  const map = L.map("mapa").setView([lat, lng], zoom);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(`
+        <h2 class="mapa__heading">DevWebCamp</h2>
+        <p class="mapa__texto">Centro de Conveciones de México</p>
+        
+    `)
+    .openPopup();
+}
